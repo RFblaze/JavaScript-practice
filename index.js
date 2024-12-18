8b625e64c9323818a0c9695e04d1d876954aa49f
Angelo Troncone <angelotroncone@gmail.com>
2024-12-18 16:26:53 -0700

    Simple todo lists (school, home) w/o checkboxes

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
-let firstName = "Hi";
-console.log(firstName);
+const schoolSubmit = document.getElementById("schoolSubmit");
+const schoolList = document.getElementById("schoolList");
+const schoolInput = document.getElementById("schoolInput");
+
+schoolSubmit.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    if (schoolInput.value != ""){
+        const newTodo = document.createElement("li");
+        newTodo.innerHTML = schoolInput.value;
+        schoolList.appendChild(newTodo);
+
+        schoolInput.value = "";
+    }
+    
+})
+
+const homeSubmit = document.getElementById("homeSubmit");
+const homeList = document.getElementById("homeList");
+const homeInput = document.getElementById("homeInput");
+
+homeSubmit.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    if (homeInput.value != ""){
+        const newTodo = document.createElement("li");
+        newTodo.innerHTML = homeInput.value;
+        homeList.appendChild(newTodo);
+
+        homeInput.value = "";
+    }
+    
+
+})
+
+
