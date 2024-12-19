1d027bcb39651404d5c6efd45aa73ed1a522f7e9
Angelo Troncone <angelotroncone@gmail.com>
2024-12-19 15:05:37 -0700

    Refactored saving todos into one function to make it DRY

--- index.js
+++ index.js
@@ -2,27 +2,52 @@ const schoolSubmit = document.getElementById("schoolSubmit");
 const schoolList = document.getElementById("schoolList");
 const schoolInput = document.getElementById("schoolInput");
 
+const saveTodo = (listName) => {
+    const newTodo = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
+    newTodo.style.listStyleType = "none"; // Remove the bullet point
+    
+
+    switch (listName) {
+        case "school":
+            newTodo.textContent = ` ${schoolInput.value}`;
+            newTodo.prepend(checkbox);
+            schoolList.appendChild(newTodo);
+
+            const savedSchoolTodos = JSON.parse(localStorage.getItem("schoolTodos")) || [];
+            savedSchoolTodos.push(schoolInput.value);
+            localStorage.setItem("schoolTodos", JSON.stringify(savedSchoolTodos));
+
+            schoolInput.value = "";
+            break;
+        
+        case "home":
+            newTodo.textContent = ` ${homeInput.value}`;
+            newTodo.prepend(checkbox);
+            homeList.appendChild(newTodo);
+
+            // Save to localStorage
+            const savedHomeTodos = JSON.parse(localStorage.getItem("homeTodos")) || [];
+            // || [] is to return an empty array if JSON.parse returns null
+
+            savedHomeTodos.push(homeInput.value);
+            localStorage.setItem("schoolTodos", JSON.stringify(savedHomeTodos));
+
+            homeInput.value = "";
+            break;
+        default:
+            break;
+    }
+}
+
+
 schoolSubmit.addEventListener("click", (event) => {
     event.preventDefault();
 
     if (schoolInput.value != "") {
-        const newTodo = document.createElement("li");
-        const checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-
-        newTodo.style.listStyleType = "none"; // Remove the bullet point
-        newTodo.textContent = ` ${schoolInput.value}`;
-        newTodo.prepend(checkbox);
-        
-        schoolList.appendChild(newTodo);
-
-        // Save to localStorage
-        const savedSchoolTodos = JSON.parse(localStorage.getItem('schoolTodos')) || [];
-        // || [] is to return an empty array if JSON.parse returns null
-        savedSchoolTodos.push(schoolInput.value);
-        localStorage.setItem("schoolTodos", JSON.stringify(savedSchoolTodos));
-
-        schoolInput.value = "";
+        saveTodo("school");
     }
     
 })
@@ -64,22 +89,7 @@ homeSubmit.addEventListener("click", (event) => {
     event.preventDefault();
 
     if (homeInput.value != "") {
-        const newTodo = document.createElement("li");
-        const checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-
-        newTodo.style.listStyleType = "none"; // Remove the bullet point
-        newTodo.innerHTML = ` ${homeInput.value}`;
-        newTodo.prepend(checkbox);
-        homeList.appendChild(newTodo);
-
-        // Save to memory
-        const savedHomeTodos = JSON.parse(localStorage.getItem("homeTodos")) || [];
-        savedHomeTodos.push(homeInput.value);
-
-        localStorage.setItem("homeTodos", JSON.stringify(savedHomeTodos));
-
-        homeInput.value = "";
+        saveTodo("home")
     }
 })
 
