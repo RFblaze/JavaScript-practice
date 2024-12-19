41387412ea041b8115c9945a271f7e034703ff7b
Angelo Troncone <angelotroncone@gmail.com>
2024-12-18 21:28:53 -0700

    Added local storage for todos, except not checkmark status

--- index.js
+++ index.js
@@ -11,14 +11,50 @@ schoolSubmit.addEventListener("click", (event) => {
         checkbox.type = "checkbox";
 
         newTodo.style.listStyleType = "none"; // Remove the bullet point
-        newTodo.innerHTML = ` ${schoolInput.value}`;
+        newTodo.textContent = ` ${schoolInput.value}`;
         newTodo.prepend(checkbox);
+        
         schoolList.appendChild(newTodo);
+
+        // Save to localStorage
+        const savedSchoolTodos = JSON.parse(localStorage.getItem('schoolTodos')) || [];
+        // || [] is to return an empty array if JSON.parse returns null
+        savedSchoolTodos.push(schoolInput.value);
+        localStorage.setItem("schoolTodos", JSON.stringify(savedSchoolTodos));
 
         schoolInput.value = "";
     }
     
 })
+
+// Load saved todos on page load
+document.addEventListener('DOMContentLoaded', () => {
+    const savedSchoolTodos = JSON.parse(localStorage.getItem('schoolTodos')) || [];
+    const savedHomeTodos = JSON.parse(localStorage.getItem("homeTodos")) || [];
+    // || [] is to return an empty array if JSON.parse returns null
+
+    savedSchoolTodos.forEach( (todo) => {
+        const newTodo = document.createElement("li");
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        newTodo.style.listStyle = "none";
+
+        newTodo.textContent = ` ${todo}`;
+        newTodo.prepend(checkbox);
+        schoolList.appendChild(newTodo);
+    });
+
+    savedHomeTodos.forEach( (todo) => {
+        const newTodo = document.createElement("li");
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        newTodo.style.listStyle = "none";
+
+        newTodo.textContent = ` ${todo}`;
+        newTodo.prepend(checkbox);
+        homeList.appendChild(newTodo);
+    });
+});
 
 const homeSubmit = document.getElementById("homeSubmit");
 const homeList = document.getElementById("homeList");
@@ -37,12 +73,14 @@ homeSubmit.addEventListener("click", (event) => {
         newTodo.prepend(checkbox);
         homeList.appendChild(newTodo);
 
+        // Save to memory
+        const savedHomeTodos = JSON.parse(localStorage.getItem("homeTodos")) || [];
+        savedHomeTodos.push(homeInput.value);
+
+        localStorage.setItem("homeTodos", JSON.stringify(savedHomeTodos));
+
         homeInput.value = "";
     }
-    
-    
-    
-
 })
 
 
